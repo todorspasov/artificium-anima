4371df4ffd284c871d87626ec294b644d18a6444
Todor Spasov <todorspasov@gmail.com>
2023-07-05 22:56:17 +0300

    Initial project structure

---
+++ src/main/java/com/tspasov/artificiumanima/ArtificiumAnimaApplication.java
@@ -0,0 +1,13 @@
+package com.tspasov.artificiumanima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificiumAnimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtificiumAnimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tspasov/artificiumanima/ArtificiumAnimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tspasov.artificiumanima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtificiumAnimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
