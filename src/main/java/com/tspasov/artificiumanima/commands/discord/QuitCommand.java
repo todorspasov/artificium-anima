c389f4260e07dad1cba03d92057d5db1631a411d
Todor Spasov <todorspasov@gmail.com>
2023-07-15 17:18:22 +0300

    Adding !quit command

---
+++ src/main/java/com/tspasov/artificiumanima/commands/discord/QuitCommand.java
@@ -0,0 +1,40 @@
+package com.tspasov.artificiumanima.commands.discord;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import com.tspasov.artificiumanima.commands.Command;
+import com.tspasov.artificiumanima.service.discord.DiscordService;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
+
+@Slf4j
+@Component
+public class QuitCommand implements Command<Message> {
+
+  private static final String QUIT_COMMAND_KEY = "!quit";
+  private static final String QUIT_COMMAND_INFO = "Ask the Artificial Oracle to disappear";
+
+  private final DiscordService discordService;
+
+  @Autowired
+  public QuitCommand(@Lazy DiscordService discordService) {
+    this.discordService = discordService;
+  }
+
+  @Override
+  public void execute(String commandStr, Message message) {
+    log.info("Quitting discord. Args: {}", commandStr);
+    this.discordService.shutDownBot();
+  }
+
+  @Override
+  public String getCommandKey() {
+    return QUIT_COMMAND_KEY;
+  }
+
+  @Override
+  public String getCommandInfo() {
+    return QUIT_COMMAND_INFO;
+  }
+}
